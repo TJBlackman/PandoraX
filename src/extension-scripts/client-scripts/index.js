64a15ea0a2ac206de024c4a62554ba1c4f6552f5
Trevor Blackman <TJBlackman08@gmail.com>
2020-04-18 07:05:59 -0600

    prettier

--- src/extension-scripts/client-scripts/index.js
+++ src/extension-scripts/client-scripts/index.js
@@ -1,18 +1,17 @@
-window.addEventListener("load", function () {
+window.addEventListener('load', function () {
   const extensionId = chrome.runtime.id;
 
-  const getLastAudioTag = () =>
-    document.querySelector("body > audio:last-of-type");
+  const getLastAudioTag = () => document.querySelector('body > audio:last-of-type');
 
   const checkIfIsAd = () => {
     try {
-      const element = document.querySelector(".nowPlayingTopInfo");
+      const element = document.querySelector('.nowPlayingTopInfo');
       if (!element) {
         return false;
       }
-      const isAd = element.className.includes("nowPlayingTopInfo--audioAd");
+      const isAd = element.className.includes('nowPlayingTopInfo--audioAd');
       const messageFromPandora = element.querySelector('.Marquee__wrapper__content').innerText === 'Message From';
-      return (isAd || messageFromPandora);
+      return isAd || messageFromPandora;
     } catch (err) {
       console.log(err);
       return false;
@@ -83,22 +82,22 @@ window.addEventListener("load", function () {
     },
     download: function () {
       let song = {
-        songName: "Unknown song",
-        artist: "Unknown Artist",
-        src: undefined
+        songName: 'Unknown song',
+        artist: 'Unknown Artist',
+        src: undefined,
       };
       try {
         song = getSongData();
       } catch (err) {
         console.log(err);
       }
-      console.log("download", song);
+      console.log('download', song);
       // send to bg script
       chrome.runtime.sendMessage(extensionId, {
-        type: "download this song",
-        payload: song
+        type: 'download this song',
+        payload: song,
       });
-    }
+    },
   };
 
   const getSongData = () => {
@@ -108,35 +107,24 @@ window.addEventListener("load", function () {
         return;
       }
 
-      const nowPlayingCenterWrapper = document.querySelector(
-        ".NowPlaying__centerWrapper"
-      );
-      const stationName = nowPlayingCenterWrapper.querySelector(
-        ".NowPlayingTopInfoSessionName__link"
-      ).innerText;
+      const nowPlayingCenterWrapper = document.querySelector('.NowPlaying__centerWrapper');
+      const stationName = nowPlayingCenterWrapper.querySelector('.NowPlayingTopInfoSessionName__link').innerText;
       const songName = (
-        nowPlayingCenterWrapper.querySelector(".Marquee__wrapper__content") ||
-        nowPlayingCenterWrapper.querySelector(
-          ".Marquee__wrapper__content__child"
-        )
+        nowPlayingCenterWrapper.querySelector('.Marquee__wrapper__content') ||
+        nowPlayingCenterWrapper.querySelector('.Marquee__wrapper__content__child')
       ).innerText;
-      const artistName = nowPlayingCenterWrapper.querySelector(
-        ".NowPlayingTopInfo__current__artistName"
-      ).innerText;
-      const albumName = nowPlayingCenterWrapper.querySelector(
-        ".nowPlayingTopInfo__current__albumName"
-      ).innerText;
+      const artistName = nowPlayingCenterWrapper.querySelector('.NowPlayingTopInfo__current__artistName').innerText;
+      const albumName = nowPlayingCenterWrapper.querySelector('.nowPlayingTopInfo__current__albumName').innerText;
       const albumArt = nowPlayingCenterWrapper
-        .querySelector(".nowPlayingTopInfo__artContainer__art")
-        .style.backgroundImage.replace(/url\("|"\)/g, "");
+        .querySelector('.nowPlayingTopInfo__artContainer__art')
+        .style.backgroundImage.replace(/url\("|"\)/g, '');
       const isThumbsUp = document
         .querySelector('[data-qa="thumbs_up_button"]')
-        .className.includes("ThumbUpButton--active");
+        .className.includes('ThumbUpButton--active');
       const isThumbsDown = document
         .querySelector('[data-qa="thumbs_down_button"]')
-        .className.includes("ThumbUpButton--active");
-      const volume = document.querySelector('[data-qa="volume_slider_handle"')
-        .attributes["aria-valuenow"].value;
+        .className.includes('ThumbUpButton--active');
+      const volume = document.querySelector('[data-qa="volume_slider_handle"').attributes['aria-valuenow'].value;
 
       const audioTag = getLastAudioTag();
       return {
@@ -151,7 +139,7 @@ window.addEventListener("load", function () {
         currentTime: audioTag.currentTime,
         duration: audioTag.duration,
         paused: audioTag.paused,
-        src: audioTag.src
+        src: audioTag.src,
       };
     } catch (err) {
       console.log(err);
@@ -161,33 +149,31 @@ window.addEventListener("load", function () {
   const refreshCurrentSongInfo = () => {
     const songData = getSongData();
     chrome.runtime.sendMessage(extensionId, {
-      type: "new song",
-      payload: songData
+      type: 'new song',
+      payload: songData,
     });
   };
 
   // listen for when song changes, send new song data to extension
   (function attachSongChangeListener() {
     try {
-      const observer_target = document.querySelector(
-        ".nowPlayingTopInfo__artContainer"
-      ).parentElement;
-      const observer = new MutationObserver(mutations => {
+      const observer_target = document.querySelector('.nowPlayingTopInfo__artContainer').parentElement;
+      const observer = new MutationObserver((mutations) => {
         refreshCurrentSongInfo();
       });
 
       const observer_options = {
         childList: false,
         subtree: true,
-        attributes: true
+        attributes: true,
       };
       observer.observe(observer_target, observer_options);
-      console.log("watching for song change");
+      console.log('watching for song change');
 
       const songData = getSongData();
       chrome.runtime.sendMessage(extensionId, {
-        type: "new song",
-        payload: songData
+        type: 'new song',
+        payload: songData,
       });
     } catch (err) {
       return setTimeout(attachSongChangeListener, 1000);
@@ -197,14 +183,14 @@ window.addEventListener("load", function () {
   // listen for "Are you still listening" popup
   (function attachStillListeningListener() {
     try {
-      const observer = new MutationObserver(arrayOfMutations => {
+      const observer = new MutationObserver((arrayOfMutations) => {
         let btn = document.querySelector('[data-qa="keep_listening_button"]');
         if (btn) {
           btn.click();
         }
       });
 
-      const observer_target = document.querySelector(".region-overlay");
+      const observer_target = document.querySelector('.region-overlay');
       const observer_options = { childList: true, subtree: true };
 
       observer.observe(observer_target, observer_options);
@@ -216,28 +202,26 @@ window.addEventListener("load", function () {
   // listen for audio ads
   (function attachAdListener() {
     try {
-      const observer = new MutationObserver(arrayOfMutations => {
+      const observer = new MutationObserver((arrayOfMutations) => {
         const isAd = checkIfIsAd();
         if (!isAd) {
           return;
         }
 
-        const addedAudioElement = arrayOfMutations.some(mutation =>
-          Array.from(mutation.addedNodes).some(
-            node => node.tagName.toLowerCase() === "audio"
-          )
+        const addedAudioElement = arrayOfMutations.some((mutation) =>
+          Array.from(mutation.addedNodes).some((node) => node.tagName.toLowerCase() === 'audio')
         );
         if (!addedAudioElement) {
           return;
         }
 
         const skipAudioAd = () => {
-          PandoraCommands.skip();
-          audioTag.removeEventListener("loadedmetadata", skipAudioAd, false);
+          setTimeout(PandoraCommands.skip, 100);
+          audioTag.removeEventListener('loadedmetadata', skipAudioAd, false);
         };
 
         const audioTag = getLastAudioTag();
-        audioTag.addEventListener("loadedmetadata", skipAudioAd, false);
+        audioTag.addEventListener('loadedmetadata', skipAudioAd, false);
       });
 
       observer.observe(document.body, { childList: true });
@@ -248,17 +232,17 @@ window.addEventListener("load", function () {
 
   // listen for video ads
   (function attachVideoAdListener() {
-    const target = document.getElementById("adContainer");
+    const target = document.getElementById('adContainer');
     try {
-      const observer = new MutationObserver(arrayOfMutations => {
+      const observer = new MutationObserver((arrayOfMutations) => {
         const videoTags = target.querySelectorAll('[title="Advertisement"]');
-        videoTags.forEach(video => {
+        videoTags.forEach((video) => {
           const skipVideoAd = () => {
             video.volume = 0;
             video.currentTime = video.duration;
-            video.removeEventListener("play", skipVideoAd, false);
+            video.removeEventListener('play', skipVideoAd, false);
           };
-          video.addEventListener("play", skipVideoAd, false);
+          video.addEventListener('play', skipVideoAd, false);
         });
       });
 
@@ -271,48 +255,49 @@ window.addEventListener("load", function () {
   // extension listener
   chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     switch (request.type) {
-      case "GET SONG INFO": {
+      case 'GET SONG INFO': {
         const songData = getSongData();
         sendResponse(songData);
         return true;
       }
-      case "replay": {
+      case 'replay': {
         PandoraCommands.replay();
         refreshCurrentSongInfo();
         break;
       }
-      case "pause": {
+      case 'pause': {
         PandoraCommands.pause();
         refreshCurrentSongInfo();
         break;
       }
-      case "play": {
+      case 'play': {
         PandoraCommands.play();
         refreshCurrentSongInfo();
         break;
       }
-      case "next": {
+      case 'next': {
         PandoraCommands.skip();
         break;
       }
-      case "thumbsup": {
+      case 'thumbsup': {
         PandoraCommands.thumbsup();
         refreshCurrentSongInfo();
         break;
       }
-      case "thumbsdown": {
+      case 'thumbsdown': {
         PandoraCommands.thumbsdown();
         break;
       }
-      case "download": {
+      case 'download': {
         PandoraCommands.download();
         break;
       }
-      case "scrub": {
+      case 'scrub': {
         PandoraCommands.scrub(request.payload); // time in seconds
         break;
       }
-      default: { };
+      default: {
+      }
     }
     sendResponse({});
     return true;
