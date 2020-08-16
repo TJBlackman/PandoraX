04454dc1407c9893383a7383ea53e269249894f5
Trevor Blackman <TJBlackman08@gmail.com>
2020-08-15 21:43:45 -0600

    feat: Pandora X website

--- src/extension-scripts/background-scripts/index.js
+++ src/extension-scripts/background-scripts/index.js
@@ -1,16 +1,18 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   switch (request.type) {
-    case "download this song": {
+    case 'download this song': {
       const filename = `Pandora Music/${request.payload.songName} by ${request.payload.artist} on album ${request.payload.album}.m4a`;
       const strippedFileName = filename.replace(/\:/g, '');
-
-      chrome.downloads.download({
+      const downloadObject = {
         url: request.payload.src,
         filename: strippedFileName,
-        saveAs: false
-      });
+        saveAs: false,
+      };
+      console.log(downloadObject);
+      chrome.downloads.download(downloadObject);
     }
-    default: { }
+    default: {
+    }
   }
   sendResponse({});
   return true;
