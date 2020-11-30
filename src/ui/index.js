e258effa502259e7ab741f1ea24b40a47a36357a
Trevor Blackman <TJBlackman08@gmail.com>
2020-11-30 08:14:06 -0700

    feat: add prettier and eslint

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Index } from "./pages/index";
 import { Settings } from "./pages/settings";
